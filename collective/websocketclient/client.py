716560c2de920bc4a06a2f8c3276266ba210605d
Petri Savolainen <petri.savolainen@koodaamo.fi>
2015-09-25 14:55:07 +0300

    improvements to connection handling

--- collective/websocketclient/client.py
+++ collective/websocketclient/client.py
@@ -1,24 +1,42 @@
-from websocket import create_connection
-
+from websocket import create_connection, setdefaulttimeout
+from websocket._exceptions import WebSocketConnectionClosedException
 
 class WebSocketConnection():
 
    TIMEOUT = 5
 
-   def __init__(self, name, host, port):
+   def __init__(self, name, host, port, timeout=None):
       self.name = name
       self.host = host
       self.port = port
+      self.timeout = timeout
+      self.active = False
 
    def connect(self):
-      self.connection = create_connection("ws://%s:%s" % (self.host, self.port))
+      connstr = "ws://%s:%s" % (self.host, self.port)
+      self.connection = create_connection(connstr, timeout=self.timeout)
+      self.active = True
 
    def send(self, data):
-      return self.connection.send(data)
+      try:
+         return self.connection.send(data)
+      except:
+         self.connection = None
+         self.active = False
+         raise
 
    def receive(self):
-      return self.connection.recv()
+      try:
+         return self.connection.recv()
+      except WebSocketConnectionClosedException:
+         self.connection = None
+         self.active = False
+         raise
 
    def close(self):
       self.connection.close()
+      self.connection = None
+      self.active = False
 
+   def settimeout(self, seconds):
+      self.timeout = timeout
