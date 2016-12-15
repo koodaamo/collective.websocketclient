97d3b553b0e86f8141e2aeef300e3def4e14d495
Petri Savolainen <petri.savolainen@koodaamo.fi>
2016-12-15 08:17:13 +0200

    make various WebSocket exceptions available for use

--- collective/websocketclient/__init__.py
+++ collective/websocketclient/__init__.py
@@ -1,9 +1,16 @@
 # -*- extra stuff goes here -*-
 
 from zope.i18n import MessageFactory
+from websocket._exceptions import WebSocketException, \
+                                  WebSocketTimeoutException, \
+                                  WebSocketConnectionClosedException
 
 _ = MessageFactory("collective.websocketclient")
 
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+
+
+class WebSocketConfigException(WebSocketException):
+   "raised when there is a problem with configuring the connection"
